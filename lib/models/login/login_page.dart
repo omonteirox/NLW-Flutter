afef77fc2c4a526e672b362a039b6ebdc58107a5
Gustavo Monteiro <gussmonteiro@gmail.com>
2021-06-24 19:59:47 -0300

    Criando o Login Controller

--- lib/models/login/login_page.dart
+++ lib/models/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/models/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size =
@@ -38,34 +41,33 @@ class _LoginPageState extends State<LoginPage> {
                   height: 300,
                 )),
             Positioned(
-                bottom: size.height * 0.15,
-                left: 0,
-                right: 0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(AppImages.logomini),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 30, left: 70, right: 70),
-                      child: Text(
-                        "Organize seus boletos em um só lugar",
-                        style: AppTextStyles.titleHome,
-                        textAlign: TextAlign.center,
-                      ),
+              bottom: size.height * 0.15,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      style: TextStyles.titleHome,
+                      textAlign: TextAlign.center,
                     ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 40, right: 40, top: 40),
-                      child: SocialLoginButton(
-                        onTap: () {
-                          print("Clicou");
-                        },
-                      ),
-                    )
-                  ],
-                ))
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(onTap: () {
+                      controller.googleSignIn(context);
+                    }),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),

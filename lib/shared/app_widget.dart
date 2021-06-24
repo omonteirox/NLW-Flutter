97ff56e636c8944efa376608717c6bef0a44f940
Gustavo Monteiro <gussmonteiro@gmail.com>
2021-06-24 20:01:01 -0300

    Criando o sistema de rotas

---
+++ lib/shared/app_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/models/home/home_page.dart';
+import 'package:payflow/models/login/login_page.dart';
+import 'package:payflow/models/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}

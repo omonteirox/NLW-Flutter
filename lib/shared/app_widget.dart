5f4bb28bc5a8ed9a81c929e39c7dc861696c6bed
Gustavo Monteiro <gussmonteiro@gmail.com>
2021-06-24 23:42:48 -0300

    Criando os widgets

--- lib/shared/app_widget.dart
+++ lib/shared/app_widget.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/models/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/models/home/home_page.dart';
 import 'package:payflow/models/login/login_page.dart';
 import 'package:payflow/models/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
-      theme: ThemeData(primaryColor: AppColors.primary),
+      theme: ThemeData(
+          primaryColor: AppColors.primary, primarySwatch: Colors.orange),
       initialRoute: "/splash",
       routes: {
         "/splash": (context) => SplashPage(),
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage(),
       },
     );
   }
